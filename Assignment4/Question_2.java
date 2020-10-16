5d86a93bba5e0aee577773c027720a66e7bc7a72
Amit-Singh <62666888+amit1916@users.noreply.github.com>
2020-10-16 21:45:48 +0530

    Program Upload

---
+++ Assignment4/Question_2.java
@@ -0,0 +1,9 @@
+public class Question_2 {
+    public static void main(String[] args) {
+        int inti = -1;
+        byte  p= (byte)inti;
+        char  q = (char)p;
+        int   r = (int)q;
+        System.out.println(r);
+    }
+}
